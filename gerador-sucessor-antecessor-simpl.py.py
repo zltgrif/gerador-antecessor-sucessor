355c4d96516c81e21e98c0395c3d078ea591a830
Leticia <lsgrifoni3@gmail.com>
2026-07-30 18:32:19 -0300

    Versão Simplificada

---
+++ gerador-sucessor-antecessor-simpl.py.py
@@ -0,0 +1,9 @@
+num = float(input('Digite um valor: '))
+antecessor = num - 1
+sucessor = num + 1
+
+print('O sucessor de {}, é: {} e o antecessor é: {}'.format(num, antecessor, sucessor))
+
+#ou:
+#num = float(input('Digite um valor: '))
+#print('O sucessor de {}, é: {} e o antecessor é: {}'.format(num, (num+1), (num-1)))
